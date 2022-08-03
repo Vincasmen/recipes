81ebb83f64351903f1d3e7e96900b89cea0198cc
Vincasmen <vincasmen13@gmail.com>
2022-08-03 20:07:27 +0200

    Deleted unuse references

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import styled from "styled-components";
-import { motion } from "framer-motion";
-import { useParams, Link } from "react-router-dom";
+// import { motion } from "framer-motion";
+import { useParams } from "react-router-dom";
 
 function Cuisine() {
   const [cuisine, setCuisine] = useState([]);
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,6 +1,6 @@
 import Home from "./Home";
 import Cuisine from "./Cuisine";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 function Pages() {
   return (
